c153dc19cf12666bc887d8639f0614c377a05fc5
Windwaker02 <woodringg95@gmail.com>
2019-06-30 18:55:34 -0400

    started on SongSearch, this will be a tab as well as something that can be used within the playlist creation page

---
+++ src/components/Pages/EditPlaylist.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class EditPlaylist extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            playlist_name: '',
+            image_url: '',
+            song_search: ''
+        }
+    }
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default EditPlaylist;
